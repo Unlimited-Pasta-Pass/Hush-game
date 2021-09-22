c14c63b2e7f1e948e46eb7e9ab2050488b99ff14
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-09-21 20:13:17 -0400

    Added floor texture

--- Hush/Assets/Scripts/PlayerCameraController.cs
+++ Hush/Assets/Scripts/PlayerCameraController.cs
@@ -2,11 +2,17 @@ using Cinemachine;
 using Helpers;
 using MLAPI;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCameraController : NetworkBehaviour
 {
+    public float cameraSpeed;
+    
     public NetworkObject networkObject;
     public GameObject lookAtTarget;
+
+    private CinemachineFreeLook _freeLookCamera;
+    private Vector2 _lookDirection;
 
     public override void NetworkStart()
     {
@@ -14,18 +20,27 @@ public class PlayerCameraController : NetworkBehaviour
         if (!networkObject.IsLocalPlayer)
             return;
 
-        CinemachineFreeLook freeLookCamera = CameraHelper.FindFreeLookCamera();
-        freeLookCamera.Follow = transform;
-        freeLookCamera.LookAt = lookAtTarget.transform;
+        _freeLookCamera = CameraHelper.FindFreeLookCamera();
+        _freeLookCamera.Follow = transform;
+        _freeLookCamera.LookAt = lookAtTarget.transform;
         
         // Disable the camera
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        _lookDirection = context.ReadValue<Vector2>();
+    }
+
+    private void ChangePlayerOrientation(Vector2 direction)
+    {
+        transform.Rotate(new Vector3(0f, 1f, 0f), direction.x * cameraSpeed * Time.deltaTime);
+    }
+    
     private void Update()
     {
-        // TODO Move the player in the direction the camera is pointing at
-        // use _camera.m_XAxis.Value to rotate the player in the right direction
+        ChangePlayerOrientation(_lookDirection);
     }
 }
