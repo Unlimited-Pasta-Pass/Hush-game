eac47fd01f792efdffab641b5bfd049e6164d6e0
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-09-28 21:49:13 -0400

    Added Static Shared State

--- Hush/Assets/Scripts/Player/PlayerMovementController.cs
+++ Hush/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,5 +1,6 @@
 using Enums;
 using MLAPI;
+using Player.Helpers;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -24,24 +25,28 @@ public class PlayerMovementController : MonoBehaviour
     public NetworkObject networkObject;
     public CharacterController characterController;
     public PlayerInput playerInput;
-    
-    // Player Speed
-    private Vector2 _moveDirection;
-    private float _desiredForwardSpeed;
-    private float _desiredLateralSpeed;
-    private float _actualForwardSpeed;
-    private float _actualLateralSpeed;
-    
-    // Input States
-    private bool _sprinting;
-    private bool _crouching;
+        
+    // Computed getters
+    private Vector3 MovementVelocity => new Vector3(_state.ActualForwardSpeed, 0, _state.ActualLateralSpeed);
+    private bool IsMoveInput => !Mathf.Approximately(_state.MoveDirection.sqrMagnitude, 0f);
+    private bool IsCharacterSprinting => _state.ActualForwardSpeed > 0 && MovementVelocity.magnitude > walkSpeed + (runSpeed - walkSpeed) / 4;
+    private bool IsCharacterSliding
+    {
+        get {
+            var animState = animator.GetCurrentAnimatorStateInfo(0);
+            return IsCharacterSprinting && (_state.Crouching || (animState.IsName(PlayerAnimator.State.Slide) && animState.normalizedTime < 0.75));
+        }
+    }
+
+    // Shared Player State
+    private CharacterState _state;
 
     // Called to update 2-axis movement input
     public void OnMove(InputAction.CallbackContext context)
     {
         if (networkObject.IsLocalPlayer)
         {
-            _moveDirection = context.ReadValue<Vector2>();
+            _state.MoveDirection = context.ReadValue<Vector2>();
         }
     }
 
@@ -50,7 +55,7 @@ public class PlayerMovementController : MonoBehaviour
     {
         if (networkObject.IsLocalPlayer)
         {
-            _sprinting = context.ReadValueAsButton();
+            _state.Sprinting = context.ReadValueAsButton();
         }
     }
 
@@ -59,20 +64,17 @@ public class PlayerMovementController : MonoBehaviour
     {
         if (networkObject.IsLocalPlayer)
         {
-            _crouching = context.ReadValueAsButton();
+            _state.Crouching = context.ReadValueAsButton();
         }
     }
-
-    // Computed getters
-    private bool IsMoveInput => !Mathf.Approximately(_moveDirection.sqrMagnitude, 0f);
-    private bool IsSprinting => _actualForwardSpeed > 0 && MovementVelocity.magnitude > walkSpeed + (runSpeed - walkSpeed) / 4;
-
-    private Vector3 MovementVelocity => new Vector3(_actualForwardSpeed, 0, _actualLateralSpeed);
 
     private void Start()
     {
         // Set the right control scheme
         playerInput.SwitchCurrentControlScheme(ControlSchemes.KeyboardAndMouse);
+        
+        // Initialize the character state variable
+        _state = CharacterState.Instance;
     }
 
     private void Update()
@@ -92,24 +94,24 @@ public class PlayerMovementController : MonoBehaviour
         float speed = GetCharacterSpeed();
         float acceleration = GetCharacterAcceleration();
         
-        _desiredForwardSpeed = _moveDirection.normalized.y * speed;
-        _desiredLateralSpeed = _moveDirection.normalized.x * speed;
+        _state.DesiredForwardSpeed = _state.MoveDirection.normalized.y * speed;
+        _state.DesiredLateralSpeed = _state.MoveDirection.normalized.x * speed;
 
-        _actualForwardSpeed = Mathf.MoveTowards(_actualForwardSpeed, _desiredForwardSpeed, acceleration * Time.deltaTime);
-        _actualLateralSpeed = Mathf.MoveTowards(_actualLateralSpeed, _desiredLateralSpeed, acceleration * Time.deltaTime);
+        _state.ActualForwardSpeed = Mathf.MoveTowards(_state.ActualForwardSpeed, _state.DesiredForwardSpeed, acceleration * Time.deltaTime);
+        _state.ActualLateralSpeed = Mathf.MoveTowards(_state.ActualLateralSpeed, _state.DesiredLateralSpeed, acceleration * Time.deltaTime);
 
         // Animate the X/Y player position
-        animator.SetFloat(PlayerAnimator.ForwardSpeed, _actualForwardSpeed);
-        animator.SetFloat(PlayerAnimator.LateralSpeed, _actualLateralSpeed);
-        animator.SetFloat(PlayerAnimator.ForwardSpeedSync, _actualForwardSpeed);
-        animator.SetFloat(PlayerAnimator.LateralSpeedSync, _actualLateralSpeed);
+        animator.SetFloat(PlayerAnimator.ForwardSpeed, _state.ActualForwardSpeed);
+        animator.SetFloat(PlayerAnimator.LateralSpeed, _state.ActualLateralSpeed);
+        animator.SetFloat(PlayerAnimator.ForwardSpeedSync, _state.ActualForwardSpeed);
+        animator.SetFloat(PlayerAnimator.LateralSpeedSync, _state.ActualLateralSpeed);
         
         // Sprint / Crouch
-        animator.SetBool(PlayerAnimator.Sprinting, IsSprinting);
-        animator.SetBool(PlayerAnimator.Crouching, _crouching);
+        animator.SetBool(PlayerAnimator.Sprinting, IsCharacterSprinting);
+        animator.SetBool(PlayerAnimator.Crouching, _state.Crouching);
         
         // Translate the player at the proper speed
-        var movement = transform.rotation * new Vector3(_actualLateralSpeed, 0f, _actualForwardSpeed);
+        var movement = transform.rotation * new Vector3(_state.ActualLateralSpeed, 0f, _state.ActualForwardSpeed);
         characterController.Move(movement / movementRatio);
     }
 
@@ -118,31 +120,24 @@ public class PlayerMovementController : MonoBehaviour
         animator.SetFloat(PlayerAnimator.ForwardSpeed, animator.GetFloat(PlayerAnimator.ForwardSpeedSync), 1.0f, Time.deltaTime * networkLerpSpeed);
         animator.SetFloat(PlayerAnimator.LateralSpeed, animator.GetFloat(PlayerAnimator.LateralSpeedSync), 1.0f, Time.deltaTime * networkLerpSpeed);
     }
-
+    
     private float GetCharacterSpeed()
     {
-        if (IsSliding())
+        if (IsCharacterSliding)
             return 0;
-        if (_crouching)
+        if (_state.Crouching)
             return crouchWalkSpeed;
-        if (_sprinting)
+        if (_state.Sprinting)
             return runSpeed;
         return walkSpeed;
     }
     
     private float GetCharacterAcceleration()
     {
-        if (IsSliding())
+        if (IsCharacterSliding)
             return slideDecelerationSpeed;
         if (IsMoveInput)
             return accelerationSpeed;
         return decelerationSpeed;
     }
-    
-    private bool IsSliding()
-    {
-        AnimatorStateInfo animState = animator.GetCurrentAnimatorStateInfo(0);
-        return IsSprinting && (_crouching || (animState.IsName(PlayerAnimator.State.Slide) && animState.normalizedTime < 0.75));
-    }
-
 }
---
+++ Hush/Assets/Scripts/Player/States/CharacterState.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player.Helpers
+{
+    public sealed class CharacterState
+    {
+        private static readonly Lazy<CharacterState> LazyState = new Lazy<CharacterState>(() => new CharacterState());
+
+        private CharacterState() { }
+        
+        public static CharacterState Instance => LazyState.Value;
+
+        #region Multithreading Locks
+        
+        private static readonly object MoveDirectionLock = new object();
+        private static readonly object DesiredForwardSpeedLock = new object();
+        private static readonly object DesiredLateralSpeedLock = new object();
+        private static readonly object ActualForwardSpeedLock = new object();
+        private static readonly object ActualLateralSpeedLock = new object();
+        private static readonly object SprintingLock = new object();
+        private static readonly object CrouchingLock = new object();
+
+        #endregion
+        
+        #region Movement States
+        
+        private Vector2 _moveDirection;
+        public Vector2 MoveDirection
+        {
+            get
+            {
+                lock (MoveDirectionLock)
+                {
+                    return _moveDirection;
+                }
+            }
+            set
+            {
+                lock (MoveDirectionLock)
+                {
+                    _moveDirection = value;
+                }
+            }
+        }
+        
+        private float _desiredForwardSpeed;
+        public float DesiredForwardSpeed
+        {
+            get
+            {
+                lock (DesiredForwardSpeedLock)
+                {
+                    return _desiredForwardSpeed;
+                }
+            }
+            set
+            {
+                lock (DesiredForwardSpeedLock)
+                {
+                    _desiredForwardSpeed = value;
+                }
+            }
+        }
+        
+        private float _desiredLateralSpeed;
+        public float DesiredLateralSpeed
+        {
+            get
+            {
+                lock (DesiredLateralSpeedLock)
+                {
+                    return _desiredLateralSpeed;
+                }
+            }
+            set
+            {
+                lock (DesiredLateralSpeedLock)
+                {
+                    _desiredLateralSpeed = value;
+                }
+            }
+        }
+        
+        private float _actualForwardSpeed;
+        public float ActualForwardSpeed
+        {
+            get
+            {
+                lock (ActualForwardSpeedLock)
+                {
+                    return _actualForwardSpeed;
+                }
+            }
+            set
+            {
+                lock (ActualForwardSpeedLock)
+                {
+                    _actualForwardSpeed = value;
+                }
+            }
+        }
+        
+        private float _actualLateralSpeed;
+        public float ActualLateralSpeed
+        {
+            get
+            {
+                lock (ActualLateralSpeedLock)
+                {
+                    return _actualLateralSpeed;
+                }
+            }
+            set
+            {
+                lock (ActualLateralSpeedLock)
+                {
+                    _actualLateralSpeed = value;
+                }
+            }
+        }
+    
+        #endregion
+        
+        #region Input States
+        
+        private bool _sprinting;
+        public bool Sprinting
+        {
+            get
+            {
+                lock (SprintingLock)
+                {
+                    return _sprinting;
+                }
+            }
+            set
+            {
+                lock (SprintingLock)
+                {
+                    _sprinting = value;
+                }
+            }
+        }
+        
+        private bool _crouching;
+        public bool Crouching{
+            get
+            {
+                lock (CrouchingLock)
+                {
+                    return _crouching;
+                }
+            }
+            set
+            {
+                lock (CrouchingLock)
+                {
+                    _crouching = value;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
