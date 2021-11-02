1e98604b56b5f2b601969892ee2761b1db54e5a6
emilie-martin <emmy.mar@hotmail.com>
2021-11-02 13:07:02 -0400

    door interaction
    animation + boolean toggling

--- Hush/Assets/Scripts/Game/DoorManager.cs
+++ Hush/Assets/Scripts/Game/DoorManager.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
-    private bool isOpen = false; // TODO: modify this value when the door operning conditionals are met
+    private bool isDoorOpen; // TODO: modify this value when the door operning conditionals are met
+    [SerializeField] private GameObject TextUI;
+    private Animator TextDoorAnimator;
+
+    void Start()
+    {
+        TextDoorAnimator = gameObject.GetComponentInChildren<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,7 +20,25 @@ public class DoorManager : MonoBehaviour
 
             if (!engagedInCombat && hasRelic)
             {
-                isOpen = true;
+                isDoorOpen = true;
+                TextDoorAnimator.SetBool("DoorIsOpen", isDoorOpen);
+                TextDoorAnimator.SetFloat("direction", 1.0f);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            bool engagedInCombat = other.gameObject.GetComponent<PlayerInputManager>().isInCombat;
+            bool hasRelic = other.gameObject.GetComponent<PlayerPossessions>().possessesRelic;
+
+            if (!engagedInCombat && hasRelic)
+            {
+                isDoorOpen = false;
+                TextDoorAnimator.SetBool("DoorIsOpen", isDoorOpen);
+                TextDoorAnimator.SetFloat("direction", -1.0f); // reverse animation
             }
         }
     }
