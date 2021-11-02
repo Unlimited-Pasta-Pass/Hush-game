94a358eba98eabce7a15d1e969a1cda63131ba7e
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2021-11-02 14:44:04 -0400

    Add spell prefab

--- Hush/Assets/Scripts/Weapon/Spell.cs
+++ Hush/Assets/Scripts/Weapon/Spell.cs
@@ -6,10 +6,14 @@ using Enums;
 public class Spell : MonoBehaviour, IWeapon
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private int spellLifetime = 5;
+    [SerializeField] private GameObject spellPrefab;
     public int CurrentDamage { get; set; }
     public int BonusDamage { get; set; }
 
     private const int SPECIAL_DAMAGE = 50;
+    
+    
 
     void Awake()
     {
@@ -19,12 +23,14 @@ public class Spell : MonoBehaviour, IWeapon
     public void PerformAttack(int damage)
     {
         animator.SetTrigger(PlayerAnimator.SpellAttack);
+        CreateSpellAttack();
         CurrentDamage = damage;
     }
 
     public void PerformSpecialAttack()
     {
         animator.SetTrigger(PlayerAnimator.SpellSpecialAttack);
+        CreateSpellAttack();
         CurrentDamage = SPECIAL_DAMAGE;
     }
 
@@ -34,5 +40,16 @@ public class Spell : MonoBehaviour, IWeapon
         {
             col.GetComponent<IEnemy>().TakeDamage(CurrentDamage);
         }
+    }
+
+    private void CreateSpellAttack()
+    {
+        
+        Vector3 SpellSpawnLocation = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        GameObject spellClone = Instantiate(spellPrefab, SpellSpawnLocation, Quaternion.identity);
+        spellClone.GetComponent<Rigidbody>().velocity = this.transform.forward;
+        Debug.Log(this.transform.forward);
+        Destroy(spellClone, spellLifetime);
+
     }
 }
