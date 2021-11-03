ebc06d757d70ef5be58b620ca72411ac90cc4020
Cristian Milatinov <milatinovcristian@gmail.com>
2021-11-03 17:42:43 -0400

    fix game ui

--- Hush/Assets/Scripts/Keys/KeyCounterUI.cs
+++ Hush/Assets/Scripts/Keys/KeyCounterUI.cs
@@ -1,17 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Text))]
 public class KeyCounterUI : MonoBehaviour
 {
-    private Text keyCountText;
-
-    void Awake () {
-        keyCountText = GetComponent<Text>();
-    }
+    [SerializeField] private TextMeshProUGUI keyCountText;
 
     void Update()
     {
         keyCountText.text = "Keys: " + GameMaster.keysInPossession.ToString();
     }
 }
+
