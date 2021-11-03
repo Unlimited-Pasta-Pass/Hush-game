ffb038dfb0b38f15625f035ed0fec178e42a6bdb
Cristian Milatinov <milatinovcristian@gmail.com>
2021-11-03 15:16:21 -0400

    Lint player animator

--- Hush/Assets/Scripts/Player/Enums/PlayerAnimator.cs
+++ Hush/Assets/Scripts/Player/Enums/PlayerAnimator.cs
@@ -5,10 +5,10 @@ namespace Enums
     public static class PlayerAnimator
     {
         public static int Speed => Animator.StringToHash("Speed");
-		public static int LightAttack => Animator.StringToHash("LightAttack");
-		public static int HeavyAttack => Animator.StringToHash("HeavyAttack");
-		public static int SpellAttack => Animator.StringToHash("SpellAttack");
-		public static int SpellSpecialAttack => Animator.StringToHash("SpellSpecialAttack");
+        public static int LightAttack => Animator.StringToHash("LightAttack");
+        public static int HeavyAttack => Animator.StringToHash("HeavyAttack");
+        public static int SpellAttack => Animator.StringToHash("SpellAttack");
+        public static int SpellSpecialAttack => Animator.StringToHash("SpellSpecialAttack");
         public static int Dead => Animator.StringToHash("Dead");
         public static class Layer
         {
