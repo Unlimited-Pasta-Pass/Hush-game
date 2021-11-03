7a1be206edefb698cabe0525d6212e67bd1397d8
arejayelle <rebecca.j.loke@gmail.com>
2021-11-03 13:35:13 -0400

    Add Pause menu script

---
+++ Hush/Assets/PauseMenu.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // brackeys tutorial: https://youtu.be/JivuXdrIHK0
+
+    public GameObject pauseMenuUI;
+    
+    // controller nav https://www.youtube.com/watch?v=SXBgBmUcTe0&ab_channel=gamesplusjames
+    public GameObject pauseFirstButton;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (TimeManager.Instance.GameIsPaused())
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        TimeManager.Instance.Pause();
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton);
+        
+        pauseMenuUI.SetActive(true);
+    }
+    
+    public void Resume()
+    {
+        Debug.Log("Resume");
+        TimeManager.Instance.Resume();
+        EventSystem.current.SetSelectedGameObject(null);
+        pauseMenuUI.SetActive(false);
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
