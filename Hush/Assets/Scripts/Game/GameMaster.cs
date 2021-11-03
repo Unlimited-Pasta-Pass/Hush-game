c5631c52259c3c7150c8185f134434cdb1c8c69f
itsShaVawn <siobhan.cousineau@gmail.com>
2021-11-02 23:02:31 -0400

    dont destroy on load

--- Hush/Assets/Scripts/Game/GameMaster.cs
+++ Hush/Assets/Scripts/Game/GameMaster.cs
@@ -18,6 +18,11 @@ public class GameMaster : MonoBehaviour
             return hasRelic;
         }
     }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
     
     // Start is called before the first frame update
     void Start()
