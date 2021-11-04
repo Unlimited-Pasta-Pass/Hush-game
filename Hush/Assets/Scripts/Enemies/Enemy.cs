7344baac2d617baedf248adbf119cb21659b6335
Cristian Milatinov <milatinovcristian@gmail.com>
2021-11-04 12:37:36 -0400

    Fix enemy aggro
    - Add layers enum
    - Ignore other enemies in LOS raycast
    - Change player sword to Default layer

--- Hush/Assets/Scripts/Enemies/Enemy.cs
+++ Hush/Assets/Scripts/Enemies/Enemy.cs
@@ -183,7 +183,10 @@ public class Enemy : MonoBehaviour, IEnemy
         var playerPosition = _player.transform.position;
         playerPosition.y = position.y;
         Vector3 toPlayer = playerPosition - position;
-        return toPlayer.magnitude <= _player.height / 2 || Physics.Raycast(position, toPlayer.normalized, out var hit, visionCollider.radius) && hit.collider.CompareTag(Tags.Player);
+        int layerMask = ~(1 << Layers.Enemy);
+        return toPlayer.magnitude <= _player.height / 2 || 
+               (Physics.Raycast(position, toPlayer.normalized, out var hit, visionCollider.radius, layerMask) && 
+               hit.collider.CompareTag(Tags.Player));
     }
     
     private void FacePlayer()
