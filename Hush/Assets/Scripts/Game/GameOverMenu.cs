0d8726f852dfce45db1b6e3b71578022b6b8855e
Siobhan <siobhan.cousineau@gmail.com>
2021-11-15 05:59:34 -0500

    added scripts for the menus

--- Hush/Assets/Scripts/Game/GameOverMenu.cs
+++ Hush/Assets/Scripts/Game/GameOverMenu.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour {
+public class GameOverMenu : MonoBehaviour {
     
+    
+    public void Rewards() {
+        // POP-UP FOR REWARDS
+    }
+
     public void Replay() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void Credits() {
+        // POP-UP FOR CREDITS
+    }
     
-    public void Quit() {
+    public void QuitGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        Debug.Log("BACK TO START MENU");
+        Debug.Log("BACK TO MAIN MENU");
     }
 
 }
---
+++ Hush/Assets/Scripts/Game/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public void Play() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Credits() {
+        // POP-UP FOR CREDITS
+    }
+
+    public void Quit() {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
