10b7bb062e8077c2d190931c3d8919de2950f721
emilie-martin <emmy.mar@hotmail.com>
2021-11-22 16:36:27 -0500

    combine menu scripts
    combined main menu script and game over script into a single menu script

--- Hush/Assets/Scripts/Game/Menus.cs
+++ Hush/Assets/Scripts/Game/Menus.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class Menus : MonoBehaviour {
     
     public void Play() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    
+    public void Replay() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
     public void Credits() {
         // POP-UP FOR CREDITS
+    }
+    
+    public void MainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("BACK TO MAIN MENU");
     }
 
     public void Quit() {
