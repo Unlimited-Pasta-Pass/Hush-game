f4d68818926cf7b8eb19a6ff1334a8acd9bbb16f
emilie-martin <emmy.mar@hotmail.com>
2021-11-22 20:50:37 -0500

    merge pause menu code into menu script

--- Hush/Assets/Scripts/Game/Menus.cs
+++ Hush/Assets/Scripts/Game/Menus.cs
@@ -1,16 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using Game;
 
-public class Menus : MonoBehaviour {
+public class Menus : MonoBehaviour
+{
     
-    public void Play() {
+    // brackeys tutorial: https://youtu.be/JivuXdrIHK0
+    public GameObject pauseMenuUI;
+    // controller nav https://www.youtube.com/watch?v=SXBgBmUcTe0&ab_channel=gamesplusjames
+    public GameObject pauseFirstButton;
+
+    public void Play()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
-    public void Replay() {
+    public void Replay()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void Pause()
+    {
+        TimeManager.Instance.Pause();
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton);
+
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resume");
+        TimeManager.Instance.Resume();
+        EventSystem.current.SetSelectedGameObject(null);
+        pauseMenuUI.SetActive(false);
     }
 
     public void Credits() {
