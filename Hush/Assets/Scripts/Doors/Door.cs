c8fe62d77a125a3beaa468dc1ad893db093ea201
emilie-martin <emmy.mar@hotmail.com>
2021-11-30 00:40:04 -0500

    added interaction ui to door prefab

--- Hush/Assets/Scripts/Doors/Door.cs
+++ Hush/Assets/Scripts/Doors/Door.cs
@@ -1,3 +1,4 @@
+using System;
 using Common.Enums;
 using Game;
 using UnityEngine;
@@ -7,6 +8,7 @@ namespace Doors
     public class Door : MonoBehaviour
     {
         [SerializeField] private GameObject wall;
+        [SerializeField] private GameObject interactionOverlay;
 
         private bool _isDoorOpen = true; // TODO: modify this value when the door opening conditionals are met
 
@@ -16,7 +18,13 @@ namespace Doors
         {
             if (!other.gameObject.CompareTag(Tags.Player)) 
                 return;
-        
+
+            if (other.gameObject.CompareTag(Tags.Player))
+            {
+                // toggle interaction script
+                transform.GetChild(0).gameObject.SetActive(true);
+            }
+
             if (CanGoThroughDoor)
             {
                 // TODO Door opening animation
@@ -24,6 +32,15 @@ namespace Doors
                 
                 // respawn back into scene
                 SceneManager.Instance.LoadNextScene();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag(Tags.Player))
+            {
+                // toggle interaction script
+                transform.GetChild(0).gameObject.SetActive(false);
             }
         }
     }
