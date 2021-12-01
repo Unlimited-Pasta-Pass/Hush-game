3bfbd949f94a9f1415895fa8cd94b650a4717ea3
Daniela Venuta <daniela.venuta27@gmail.com>
2021-12-01 00:59:20 -0500

    Add Choice selection

--- Hush/Assets/Scripts/Game/PowerUpSelectionManager.cs
+++ Hush/Assets/Scripts/Game/PowerUpSelectionManager.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class PowerUpSelectionManager : MonoBehaviour
 {
-    public static PowerUpSelectionManager Instance;
+    public enum Choice
+    {
+        Strength = 0,
+        Speed = 1,
+        Vitality = 2,
+    }
     
+    public static PowerUpSelectionManager Instance;
+    private Choice currentlySelected = 0;
+
     private void Awake()
     {
         if (Instance == null)
@@ -15,17 +23,40 @@ public class PowerUpSelectionManager : MonoBehaviour
         DontDestroyOnLoad(Instance.gameObject);
     }
 
-    public void SetupDamageBoost(float boost)
+    public void OnSubmit(float boost)
+    {
+        switch(currentlySelected)
+        {
+            case Choice.Strength:
+                SetupDamageBoost(boost);
+                break;
+            case Choice.Speed:
+                SetupSpeedBoost(boost);
+                break;
+            case Choice.Vitality:
+                SetupVitalityBoost(boost);
+                break;
+            default: // select nothing
+                break;
+        }
+    }
+
+    public void SetCurrentlySelected(Choice selected)
+    {
+        currentlySelected = selected;
+    }
+
+    private void SetupDamageBoost(float boost)
     {
         GameManager.Instance.AddToPlayerDamageBoost(boost);
     }
 
-    public void SetupSpeedBoost(float boost)
+    private void SetupSpeedBoost(float boost)
     {
         GameManager.Instance.AddToPlayerSpeedBoost(boost);
     }
 
-    public void SetupVitalityBoost(float boost)
+    private void SetupVitalityBoost(float boost)
     {
         float currentHp = GameManager.Instance.PlayerMaxHitPoints;
         float currentMax = GameManager.Instance.PlayerCurrentHitPoints;
