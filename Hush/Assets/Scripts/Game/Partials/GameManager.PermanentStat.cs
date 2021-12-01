634b8eef70875e96cf42d4fccfecaedaace56eab
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2021-12-01 15:37:14 -0500

    Add Permanent Stat Boosts to Game State (#166)

---
+++ Hush/Assets/Scripts/Game/Partials/GameManager.PermanentStat.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Schema;
+
+namespace Game
+{
+    public partial class GameManager
+    {
+        public float PermanentDamage
+        {
+            get => _state.damagePermanent;
+            set => _state.damagePermanent = value;
+        }
+        
+        public float PermanentSpeed
+        {
+            get => _state.speedPermanent;
+            set => _state.speedPermanent = value;
+        }
+        
+        public float PermanentVitality
+        {
+            get => _state.vitalityPermanent;
+            set => _state.vitalityPermanent = value;
+        }
+        
+    }
+}
