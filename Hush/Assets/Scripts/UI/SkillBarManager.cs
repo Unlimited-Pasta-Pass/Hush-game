69cc460d0967108c4aa278124254eb4b8d80b4d9
Daniela Venuta <daniela.venuta27@gmail.com>
2021-11-30 21:47:20 -0500

    Change Skillbar Sprite Depending on Active Spells

---
+++ Hush/Assets/Scripts/UI/SkillBarManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Game;
+using UnityEngine;
+using UnityEngine.UI;
+using Weapon.Enums;
+
+public class SkillBarManager : MonoBehaviour
+{
+    [SerializeField] private GameObject heavyIcon;
+    [SerializeField] private GameObject lightIcon;
+    [SerializeField] private Sprite stunSprite;
+    [SerializeField] private Sprite fireSprite;
+    [SerializeField] private Sprite invisibleSprite;
+
+    private void Start()
+    {
+        heavyIcon.GetComponent<Image>().sprite = SpellIcon(GameManager.Instance.GetActiveHeavySpell());
+        lightIcon.GetComponent<Image>().sprite = SpellIcon(GameManager.Instance.GetActiveLightSpell());
+    }
+
+    private Sprite SpellIcon(SpellType spell)
+    {
+        Sprite sprite;
+        switch (spell)
+        {
+            case SpellType.FireballSpell:
+                sprite = fireSprite;
+                break;
+            case SpellType.InvisibleSpell:
+                sprite = invisibleSprite;
+                break;
+            case SpellType.StunSpell:
+                sprite = stunSprite;
+                break;
+            default:
+                sprite = fireSprite;
+                break;
+        }
+
+        return sprite;
+    }
+}
