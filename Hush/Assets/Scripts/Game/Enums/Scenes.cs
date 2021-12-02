ff0088d62a98bb3066ee2ccd2aefd9ec0249cde8
arejayelle <rebecca.j.loke@gmail.com>
2021-12-01 19:53:04 -0500

    ACTUALLY ADD THE SCENES FILE

---
+++ Hush/Assets/Scripts/Game/Enums/Scenes.cs
@@ -0,0 +1,18 @@
+﻿namespace Game.Enums
+{
+    public static class Scenes
+    {
+        public static int MainMenu = 0;
+        public static int DevScene = 1;
+        public static int GameOver = 2; 
+        public static int SpellChoice= 3;
+        public static int PowerUp = 4;
+        public static int PermanentPower = 5; // currently just the Demo floor
+        
+        // The actual dungeon
+        public static int FirstFloor = 6;
+        public static int SecondFloor = 7;
+        public static int ThirdFloor = 8;
+
+    }
+}
