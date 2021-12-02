3100f520fe204e15446fcbb9d9c2f6013156617f
arejayelle <rebecca.j.loke@gmail.com>
2021-12-01 21:19:50 -0500

    Add file to handle transitions

---
+++ Hush/Assets/Scripts/Game/Partials/GameManager.Transitions.cs
@@ -0,0 +1,11 @@
+﻿namespace Game
+{
+    public partial class GameManager
+    {
+        public void OnPlayerDeath()
+        {
+            CurrentGameState.OnPlayerDeath();
+            SaveGameManager.Instance.OnSave();
+        }
+    }
+}
